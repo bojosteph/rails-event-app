88dfe4aa275026ae19daa8b821f91626651c449a
bojosteph <bojosteph@gmail.com>
2019-04-22 10:14:40 -0700

    added event model validation for datetime

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,9 @@ class Event < ApplicationRecord
   validates_presence_of :name, :location, :description, :start_of_event, :end_of_event
   belongs_to :user
   has_many :rsvp_events
+  validate :date_must_be_current, if: :has_date_range?
+  validate :correct_date_range, if: :has_date_range?
+  
 
   def self.active_event
       where("end_of_event > ?", DateTime.now)
@@ -9,6 +12,22 @@ class Event < ApplicationRecord
 
   def self.past_event
       where("end_of_event < ?", DateTime.now)
+  end
+
+  def has_date_range?
+    start_of_event.present? && end_of_event.present?
+  end
+
+  def date_must_be_current
+    unless start_of_event >= DateTime.now
+    errors.add(:start_of_event, "must be on or after current date")
+    end
+  end
+
+  def correct_date_range
+    unless end_of_event >= start_of_event
+      errors.add(:end_of_event, "must be on or after start of event")
+    end
   end
 
   
